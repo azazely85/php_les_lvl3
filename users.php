f8c8a8e9bad4f9410ba79af632b595ddca5c37e6
Yaroslav Cherednyk <azazely85@gmail.com>
2017-02-13 14:57:10 +0200

    fix class User

--- users.php
+++ users.php
@@ -7,20 +7,23 @@ class User{
         echo "Name: ".$this->name."<br/>";
         echo "Login: ".$this->login."<br/>";
         echo "Password: ".$this->password."<br/>";
+        echo "<hr>";
     }
 }
 $user1 = new User();
 $user1->name="Ivan";
 $user1->login="ivan2";
 $user1->password="123456";
+$user1->showInfo();
+
 $user2 = new User();
 $user2->name="Petro";
 $user2->login="petr2";
 $user2->password="123456";
+$user2->showInfo();
+
 $user3 = new User();
 $user3->name="Mark";
 $user3->login="mark85";
 $user3->password="123456";
-$user1->showInfo();
-$user2->showInfo();
 $user3->showInfo();
